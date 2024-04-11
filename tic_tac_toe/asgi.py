aa5d2e385c6bfade15de1a2f480b27242b8d8dba
utkarshbig <127543087+utkarshbig@users.noreply.github.com>
2024-04-11 19:36:58 +0530

    Add files via upload

---
+++ tic_tac_toe/asgi.py
@@ -0,0 +1,32 @@
+"""
+ASGI config for tic_tac_toe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+from home.consumers import GameRoom
+from django.core.asgi import get_asgi_application
+from django.urls import path
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tic_tac_toe.settings')
+
+application = get_asgi_application()
+
+
+ws_pattern = [
+        path('ws/game/<room_code>' , GameRoom)
+]
+
+
+application= ProtocolTypeRouter(
+    {
+        'websocket':AuthMiddlewareStack(URLRouter(
+            ws_pattern
+        ))
+    }
+)
